bbd35eb4790cbd873aabf96679075774cfd2e1b5
riera90@alpha <diegorodriguezriera@gmail.com>
2018-03-01 21:18:52 +0100

    added ej4 and gitignore

---
+++ pr1/ejercicio3.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+if [ "$1" == $"" ]; then
+	echo "error"
+else
+	dir=$1
+fi
+
+if [ "$2" == $"" ] ; then
+	low_limit=$((10000))
+else
+	low_limit=$2
+fi
+if [ "$3" == $"" ] ; then
+	top_limit=$((100000))
+else
+	top_limit=$3
+fi
+#deletes the directories if created
+if [ -d "$dir""/small" ]; then
+	echo "deleting foldet 'small'"
+	rm -r "$dir"/small
+fi
+if [ -d "$dir""/medium" ]; then
+	echo "deleting foldet 'medium'"
+	rm -r "$dir"/medium
+fi
+if [ -d "$dir""/big" ]; then
+	echo "deleting foldet 'big'"
+	rm -r "$dir"/big
+fi
+mkdir "$dir"/small
+mkdir "$dir"/medium
+mkdir "$dir"/big
+
+echo -e "dir: $dir \t low: $low_limit \t top: $top_limit"
+files=$(ls $dir)
+echo $files
+for it in $files ; do
+	origin="$dir""/""$it"
+	if [ -d $origin ]; then
+		echo -e "$it is a directory , doing nothing"
+	else
+		echo $origin
+		size=$(stat $origin --printf="%s")
+		if [ $((size)) -lt $((low_limit)) ]; then
+			#small
+			echo -e "$it es pequeño"
+			mv $origin $dir"/small/"$it
+		else
+			if [ $((size)) -lt $((top_limit)) ]; then
+				#big
+				echo -e "$it es grande"
+				mv $origin $dir"/big/"$it
+			else
+				#med
+				echo -e "$it es mediano"
+				mv $origin "$dir""/medium/""$it"
+			fi
+		fi
+	fi
+done
---
+++ pr1/ejercicio4.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+files=$(ls)
+count=$((0))
+echo $files
+for it in $files ; do
+	if [ -f $it ]; then
+		count=$((count+1))
+	fi
+done
+echo -e "hay $count ficheros en este directorio"
+echo "la lista de usuarios conectados ahora es:"
+for it in $(who | cut -d " " -f 1 | tr "" "\n") ; do
+	echo $it
+done
+echo "introdizca un caracter a contar recursivamente en los nombres de los ficheros o directorios:"
+clock=5
+while [ $((0)) -lt $((clock)) ] ; do
+	echo -e "\rtienes ${clock} segundos para introducit el caracter: \c"
+	read -t 1 character
+	clock=$((clock-1))
+	if [ ! -z "$character" ]; then
+		break;
+	fi
+done
+if [ -z "$character" ]; then
+	character="a"
+	echo "time out"
+fi
+echo -e "contando el no de $character's que aparecen"
+echo $(ls -R | tr -cd "$character" | wc -m)
