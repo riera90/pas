cdf50bfb95c7bd83ff05e0b28a2af0d313c3006b
riera90@alpha <diegorodriguezriera@gmail.com>
2018-03-01 13:29:39 +0100

    removed debug comments

--- pr1/ejercicio1.sh
+++ pr1/ejercicio1.sh
@@ -5,8 +5,7 @@ var=$((varc+varh))
 all=$(ls *.c )
 all+=" "
 all+=$(ls *.h )
-if [ $var -lt 1 ];
-then
+if [ $var -lt 1 ] ; then
 	echo "no hay archivos .c o .h en este directorio"
 else
 	top=-1
@@ -14,32 +13,19 @@ else
 	for (( i = 0; i < $var; i++ )); do
 		max_size=$((0))
 		max_file="ndef"
-
-
 		for it in $all ; do
 			size=$(cat $it | wc -m)
-			# echo -e "size to check: $size | max: $max_size | top: $top"
 			if [ $((max_size)) -lt $((size)) ] ; then
-				# echo "checking!"
 				if [[ $top -eq $((-1)) ]] ; then
-					# echo ">>>>>>>>>>>>>>>new<<<<<<<<<<<<<<<"
 					max_file=$it
 					max_size=$size
-					# echo -e "new file: $max_file"
-					# echo -e "new size: $max_size"
 				else
 					if [[ $top -gt $size ]] ; then
-						# echo ">>>>>>>>>>>>>>>new not def<<<<<<<<<<<<<<<"
 						max_file=$it
 						max_size=$size
-						# echo -e "new file: $max_file"
-						# echo -e "new size: $max_size"
 					fi
 				fi
-
 			fi
-
-
 		done
 		top=$((max_size-1))
 		echo -e "fichero: $max_file \t lineas: $(cat $max_file | wc --lines) \t caracteres: $max_size"
